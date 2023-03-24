7ec3a357c1263531b1a5605ea1c29476c4ccc323
Dani Ramirez <danirg1414@outlook.com>
2023-03-24 11:58:29 -0500

    splash-screen-v1

--- interfaces-pr/app/src/main/java/com/example/interfaces_pr/MainActivity.kt
+++ interfaces-pr/app/src/main/java/com/example/interfaces_pr/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.interfaces_pr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainHandler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainHandler.postDelayed({
+            startActivity(Intent(this, MainActivity2::class.java))
+            finish()
+        }, 4000)
+
+        
+
     }
 }
