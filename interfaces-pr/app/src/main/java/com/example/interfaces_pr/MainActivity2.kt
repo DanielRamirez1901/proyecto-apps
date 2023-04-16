848fee84be1a83abc199a913521d2e3ea36ff276
Dani Ramirez <danirg1414@outlook.com>
2023-04-16 10:45:44 -0500

    Curso_Skeleton_Added

--- interfaces-pr/app/src/main/java/com/example/interfaces_pr/MainActivity2.kt
+++ interfaces-pr/app/src/main/java/com/example/interfaces_pr/MainActivity2.kt
@@ -1,10 +1,15 @@
 package com.example.interfaces_pr
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.interfaces_pr.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -15,6 +20,8 @@ class MainActivity2 : AppCompatActivity() {
 
     private val login = LoginFragment.newInstance()
     private val register = RegisterFragment.newInstance()
+    private val _text = MutableLiveData<String>()
+    val text: LiveData<String> = _text
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +41,19 @@ class MainActivity2 : AppCompatActivity() {
             showFragment(register)
         }
 
+
+
     }
 
 
 
-    fun showFragment(fragment:Fragment){
+    private fun showFragment(fragment:Fragment){
         supportFragmentManager.beginTransaction().replace(R.id.nada,fragment).commit()
     }
+
+    fun irASegundaActivity(view: View) {
+        val intent = Intent(this, CursoActivity::class.java)
+        startActivity(intent)
+    }
+
 }
